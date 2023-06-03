f7b5445dd3b8465b7b297501418e703874c5f43b
ajitkumarpal1 <palajit29@gmail.com>
2023-06-03 21:54:02 +0530

    Add index.js

--- src/services/services.js
+++ src/services/services.js
@@ -3,20 +3,20 @@ import axios from 'axios';
 export const Services = {
     in: async (data) => {
         try {
-            const response = await axios.post("http://localhost:8000/user/create", data)
+            const response = await axios.post("http://nodecard-production-f98e.up.railway.app/user/create", data)
             return response.data; 
         } catch (error) {
             console.log("errrr", error);
         }
     },
     get: () => {
-        return axios.get("http://localhost:8000/user");
+        return axios.get("http://nodecard-production-f98e.up.railway.app/user");
     },
     delete: (id) => {
-        return axios.delete("http://localhost:8000/user/delete/" + id)
+        return axios.delete("http://nodecard-production-f98e.up.railway.app/user/delete/" + id)
     },
     edit: (id,data) => {
         console.log("1111>>>",id,data);
-        return axios.put("http://localhost:8000/user/update/"+ id,data);
+        return axios.put("http://nodecard-production-f98e.up.railway.app/user/update/"+ id,data);
     }
 };
