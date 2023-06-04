b68e76b2c922a33c47c246551561bf91c1f5d047
ajitkumarpal1 <66736656+ajitkumarpal1@users.noreply.github.com>
2023-06-04 12:53:21 +0530

    Update services.js

--- src/services/services.js
+++ src/services/services.js
@@ -3,20 +3,20 @@ import axios from 'axios';
 export const Services = {
     in: async (data) => {
         try {
-            const response = await axios.post("http://nodecard-production-f98e.up.railway.app/user/create", data)
+            const response = await axios.post("https://nodecard-production-f98e.up.railway.app/user/create", data)
             return response.data; 
         } catch (error) {
             console.log("errrr", error);
         }
     },
     get: () => {
-        return axios.get("http://nodecard-production-f98e.up.railway.app/user");
+        return axios.get("https://nodecard-production-f98e.up.railway.app/user");
     },
     delete: (id) => {
-        return axios.delete("http://nodecard-production-f98e.up.railway.app/user/delete/" + id)
+        return axios.delete("https://nodecard-production-f98e.up.railway.app/user/delete/" + id)
     },
     edit: (id,data) => {
         console.log("1111>>>",id,data);
-        return axios.put("http://nodecard-production-f98e.up.railway.app/user/update/"+ id,data);
+        return axios.put("https://nodecard-production-f98e.up.railway.app/user/update/"+ id,data);
     }
 };
